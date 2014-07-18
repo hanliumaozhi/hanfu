2ce4a9acb132a5380c3bb247b8a64f2970949143
hanliumaozhi <hanliumaozhi@gmail.com>
2014-07-19 01:11:05 +0800

    完成图片的显示

--- app/views.py
+++ app/views.py
@@ -3,8 +3,18 @@ from flask import request
 
 from app import app
 
+import json
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("index.html")
+    else:
+        print request.form['turns']
+        print request.form['onesNum']
+        dataList = list()
+        for i in xrange(int(request.form['onesNum'])):
+            dataList.append(['1', '/static/test.jpg', 'liusong', 'hanfu'])
+        
+        return json.dumps({'data':dataList})
